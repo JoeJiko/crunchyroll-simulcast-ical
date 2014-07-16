0a3580029db9069ee0babb3c9b6a9aa9287501a3
Jiko <me@joejiko.com>
2014-07-16 12:58:22 -0400

    initial commit

---
+++ dom_reference.php
@@ -0,0 +1,114 @@
+<?php
+# .portrait-grid .group-item
+// <li group_id="257143" class="hover-bubble group-item" itemtype="http://schema.org/TVSeries" itemscope="" id="media_group_257143">
+//     <div data-classes="container-shadow-dark" class="wrapper hover-toggle-queue container-shadow hover-classes">
+//       <a class="portrait-element block-link titlefix" href="/ace-of-the-diamond" itemprop="url" token="shows-portraits">
+//         <span class="img-holder"><img class="portrait" src="http://img1.ak.crunchyroll.com/i/spire4/3454db4ad48c5ff4592e5cd4d770bc4f1380139027_thumb.jpg" alt="Ace of the Diamond" itemprop="photo"></span>
+//         <span class="series-title block ellipsis" itemprop="name">Ace of the Diamond</span>
+//         <span class="series-data block ellipsis">
+//                     40 Videos                  </span>
+//       </a>
+//     <button type="button" group_id="257143" id="series_queue_257143" class="queue-button add-queue-button not-queued special-button small-button button" style="display: none;">     <span class="queue-icon"></span>     <span class="queue-label">Add to Queue</span>   </button></div>
+//   </li>
+
+# #sidebar_elements
+//         <li class="large-margin-bottom">
+//           <img itemprop="image" src="http://img1.ak.crunchyroll.com/i/spire4/3454db4ad48c5ff4592e5cd4d770bc4f1380139027_full.jpg" class="poster xsmall-margin-bottom">
+//           <span id="sharing_add_queue_button" group_id="257143"><button style="display: inline-block;" class="queue-button add-queue-button not-queued special-button small-button button" id="series_queue_257143" group_id="257143" type="button">     <span class="queue-icon"></span>     <span class="queue-label">Add to Queue</span>   </button></span>
+//         </li>
+//                         <li class="large-margin-bottom">
+//           <h3>About the Show</h3>
+//                                 <p class="strong">Simulcast on Saturdays 8:30pm EDT</p>
+//                     <p itemprop="description" class="description">
+//                                     <span class="trunc-desc">
+//                 I want to pitch to that mitt again...
+// A meeting with catcher Kazuya Miyuki changed the 15-year-old Eijun Sawamura's life.
+// He said              </span>
+//                           <span class="more" style="display:none;">
+//                 I want to pitch to that mitt again...
+// A meeting with catcher Kazuya Miyuki changed the 15-year-old Eijun Sawamura's life.
+// He said goodbye to all his friends and knocked upon the door of Seidou, a prestigious baseball school, intent on testing his own strength. There, he met many proud baseball players who were betting everything on the sport!
+// A classic tale, yet new and fresh. All the emotion and excitement of the popular baseball manga is at last coming to television in the form of an anime!              </span>
+//               <span class="more-dots">...</span>
+//               <span>
+//                 <a href="#" onclick="$('.description .trunc-desc').toggle(); $('.description .more').toggle(); $('.more-dots').toggle(); $(this).text($(this).text()==='more'?'less':'more'); return false;">more</a>
+//               </span>
+//                       </p>
+//         </li>
+//         <li class="large-margin-bottom">
+//           <h3>User Ratings</h3>
+//           <div class="average-rating cf xsmall-margin-bottom">
+//             <div class="strong left small-margin-right">Average Rating:</div> <div class="left"><span class="nonratable-parent" itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating">
+// <span id="showview_about_rate_widget" class="rating-widget-static-large" style="background-position:0px -126px;" itemprop="ratingValue" content="4.5">
+//   </span>
+//   <meta itemprop="bestRating" content="5">
+//   <meta itemprop="worstRating" content="0">
+//   </span>
+// </div>
+//           </div>
+//                     <ul class="rating-histogram list-block small-data">
+//                                       <li class="5-star cf">
+//                 <div class="left num strong">5</div>
+//                 <div class="left rating-bar xsmall-margin-right">
+//                   <div class="rating-proportion" style="width: 80.3922%;"></div>
+//                 </div>
+//                                 <div class="left">(41)</div>
+//               </li>
+//                           <li class="4-star cf">
+//                 <div class="left num strong">4</div>
+//                 <div class="left rating-bar xsmall-margin-right">
+//                   <div class="rating-proportion" style="width: 13.7255%;"></div>
+//                 </div>
+//                                 <div class="left">(7)</div>
+//               </li>
+//                           <li class="3-star cf">
+//                 <div class="left num strong">3</div>
+//                 <div class="left rating-bar xsmall-margin-right">
+//                   <div class="rating-proportion" style="width: 0%;"></div>
+//                 </div>
+//                                 <div class="left">(0)</div>
+//               </li>
+//                           <li class="2-star cf">
+//                 <div class="left num strong">2</div>
+//                 <div class="left rating-bar xsmall-margin-right">
+//                   <div class="rating-proportion" style="width: 3.92157%;"></div>
+//                 </div>
+//                                 <div class="left">(2)</div>
+//               </li>
+//                           <li class="1-star cf">
+//                 <div class="left num strong">1</div>
+//                 <div class="left rating-bar xsmall-margin-right">
+//                   <div class="rating-proportion" style="width: 1.96078%;"></div>
+//                 </div>
+//                                 <div class="left">(1)</div>
+//               </li>
+//                                       <script>
+//                 $(function () {
+//                   setTimeout(function () {
+//                                         $('#sidebar .rating-histogram .5-star .rating-proportion').animate({width: 80.392156862745 +'%'}, 1000);
+//                                         $('#sidebar .rating-histogram .4-star .rating-proportion').animate({width: 13.725490196078 +'%'}, 1000);
+//                                         $('#sidebar .rating-histogram .2-star .rating-proportion').animate({width: 3.921568627451 +'%'}, 1000);
+//                                         $('#sidebar .rating-histogram .1-star .rating-proportion').animate({width: 1.9607843137255 +'%'}, 1000);
+//                                         $('#sidebar .rating-histogram .3-star .rating-proportion').animate({width: 0 +'%'}, 1000);
+//                                       }, 750);
+//                 });
+//               </script>
+//                       </ul>
+//           <a id="sidebar_read_reviews" href="/ace-of-the-diamond/reviews" class="text-link small-margin-top block">Read Reviews</a>
+//         </li>
+//         <li class="large-margin-bottom">
+//           <h3>Details</h3>
+//                     <ul>
+//                           <li><span class="detail-heading">Publisher: </span>
+//                 <a href="/publisher/TV_Tokyo" class="text-link">TV TOKYO</a>              </li>
+//                                                               <li><span class="detail-heading">Tags: </span>
+//                                   <a href="/showtag?tag=ace+of+the+diamond&amp;type=2" class="text-link">ace of the diamond</a>,                                  <a href="/videos/anime/genres/sports" class="text-link">sports</a>,                                  <a href="/videos/anime/genres/shounen" class="text-link">shounen</a>                              </li>
+//                       </ul>
+//           <div class="cf">
+//                           <a class="series-rss small-margin-top block left small-margin-right" href="/ace-of-the-diamond.rss" title="Ace of the Diamond Videos RSS">
+//               <img src="http://static.ak.crunchyroll.com/i/beta/rss15.png" class="no-border" alt="Ace of the Diamond Videos RSS">
+//             </a>
+//                                                   <a class="text-link left small-margin-top" href="/forumcategory-575775/ace-of-the-diamond">Forum</a>
+//                       </div>
+//         </li>
+//
---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+ini_set('max_execution_time', 300);
+
+define('CRUNCHYROLL_BASE', 'http://www.crunchyroll.com');
+define('CRUNCHYROLL_SUMMER_LIST', '/videos/anime/seasons/summer-2014');
+define('EOL', "\r\n");
+
+// helper functions
+function readCSV($csvFile){
+  $file_handle = fopen($csvFile, 'r');
+  while (!feof($file_handle) ) {
+    $line_of_text[] = fgetcsv($file_handle, 1024);
+  }
+  fclose($file_handle);
+  return $line_of_text;
+}
+
+function dd($var){
+  var_dump($var);
+  die();
+}
+
+require 'vendor/phpQuery/phpQuery-onefile.php';
+
+$xcr_ol = json_decode(file_get_contents('cr_ol.json'));
+$cr_ol = [];
+$doc = phpQuery::newDocument(file_get_contents(CRUNCHYROLL_BASE . CRUNCHYROLL_SUMMER_LIST));
+// gather list of items from index
+$items = pq('ul.portrait-grid li.group-item');
+foreach($items as $item) {
+  //   $summary     - text title of the event
+  //   $datestart   - the starting date (in seconds since unix epoch)
+  //   $dateend     - the ending date (in seconds since unix epoch)
+  //   $address     - the event's address
+  //   $uri         - the URL of the event (add http://)
+  //   $description - text description of the event
+  //   $filename    - the name of this file for saving (e.g. my-event-name.ics)
+  $title = pq($item)->find('[itemprop="name"]')->html();
+  $gid = pq($item)->attr('group_id');
+
+  if(isset($xcr_ol->$gid)) {
+    $cr_ol[$gid] = $xcr_ol->$gid;
+    continue;
+  }
+
+  $data = (object) [
+    'summary' => $title,
+    'datestart' => null,
+    'dateend' => null,
+    'address' => null,
+    'uri' => CRUNCHYROLL_BASE . pq($item)->find('a')->attr('href'),
+    'description' => null
+  ];
+  $cr_ol[$gid] = $data;
+}
+
+/**
+ * collect data
+ */
+foreach($cr_ol as $i => $li) {
+
+  // skip
+  if(!is_null($li->datestart)) continue;
+
+  // fetch
+  $temp = phpQuery::newDocument(file_get_contents($li->uri));
+  phpQuery::selectDocument($temp);
+
+  $li->poster = $temp["#sidebar_elements"]->find('.poster')->attr('src');
+  $simulcastText = $temp["#sidebar_elements li:eq(1) .strong"]->html();
+  $li->datestart = strtotime(str_replace('Simulcast on ', '', $simulcastText));
+  $li->dateend = strtotime("+1 hour", $li->datestart);
+  $li->description = trim($temp['#sidebar_elements .more']->html());
+
+  // out
+  $cr_ol[$i] = $li;
+}
+
+// save for later
+file_put_contents('cr_ol.json',json_encode($cr_ol));
+
+header('Content-type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename=crunchyroll.ics');
+function dayToCal($day) {
+  switch(strtolower($day)){
+    case "sunday":
+      return "SU";
+    case "monday":
+      return "MO";
+    case "tuesday":
+      return "TU";
+    case "wednesday":
+      return "WE";
+    case "thursday":
+      return "TH";
+    case "friday":
+      return "FR";
+    case "saturday":
+      return "SA";
+  }
+}
+function dateToCal($timestamp) {
+  return date('Ymd\THis\Z', $timestamp);
+}
+
+// Escapes a string of characters
+function escapeString($string) {
+  return preg_replace('/([\,;])/','\\\$1', $string);
+}
+
+$load = "BEGIN:VCALENDAR" . EOL .
+"VERSION:2.0" . EOL .
+"PRODID:-//project/author//NONSGML v1.0//EN" . EOL .
+"CALSCALE:GREGORIAN" . EOL;
+foreach($cr_ol as $li) {
+$load .= "BEGIN:VEVENT" . EOL .
+"DTSTART:" . dateToCal($li->datestart) . EOL .
+"DTEND:" . dateToCal($li->dateend) . EOL .
+"RRULE:FREQ=WEEKLY;" . EOL .
+"DTSTAMP:" . dateToCal(time()) . EOL .
+"UID:" . md5(rand()) . EOL .
+"URL:" . htmlspecialchars($li->uri) . EOL .
+"DESCRIPTION:" . htmlspecialchars(str_replace("\r\n",'',$li->description)) . EOL .
+"SUMMARY:" . htmlspecialchars($li->summary) . EOL .
+"END:VEVENT" . EOL;
+}
+$load .= "END:VCALENDAR";
+
+echo $load;
